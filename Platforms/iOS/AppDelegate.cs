87bd49d3b747c388fb1a035a24cbab56ba9bcdb7
Jakub Šanda <jakub.sanda@principal.cz>
2024-02-29 14:46:35 +0100

    adding project

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using Microsoft.Identity.Client;
+using UIKit;
+
+namespace MsalAuthInMauiBlazor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override bool OpenUrl(UIApplication application, NSUrl url, NSDictionary options)
+        {
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(url);
+            return true;
+        }
+    }
+}
